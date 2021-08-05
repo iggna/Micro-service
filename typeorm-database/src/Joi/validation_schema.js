b66e7204bb41d617b102df0eb7fce00e9c277b50
Iggna <iggnaa@gmail.com>
2021-08-05 00:12:27 -0300

    Validation schema with JOI for post and patch req

--- typeorm-database/src/Joi/validation_schema.js
+++ typeorm-database/src/Joi/validation_schema.js
@@ -3,8 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
+exports.authEmail = exports.authSchema = void 0;
 var joi_1 = __importDefault(require("joi"));
-var authSchema = joi_1["default"].object({
+exports.authSchema = joi_1["default"].object({
     code: joi_1["default"].string().alphanum().min(8).max(8)
 });
-exports["default"] = authSchema;
+exports.authEmail = joi_1["default"].object({
+    email: joi_1["default"].string().email().required()
+});
--- typeorm-database/src/Joi/validation_schema.ts
+++ typeorm-database/src/Joi/validation_schema.ts
@@ -1,7 +1,10 @@
 import Joi from "joi";
 
-const authSchema = Joi.object({
+export const authSchema = Joi.object({
     code: Joi.string().alphanum().min(8).max(8)
 })
 
-export default authSchema
+export const authEmail = Joi.object({
+    email: Joi.string().email().required()
+})
+
