27384ecfffc573ef237fd5ed5d15aabaa5677737
Iggna <iggnaa@gmail.com>
2021-08-06 13:24:46 -0300

    const date unused deleted

--- typeorm-database/src/Rdate/randomDate.js
+++ typeorm-database/src/Rdate/randomDate.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var date_1 = __importDefault(require("@js-random/date"));
-var date = date_1["default"]();
 var Rdate = date_1["default"]({
     from: new Date(2021, 8, 8),
     to: new Date(2022, 8, 8)
--- typeorm-database/src/Rdate/randomDate.ts
+++ typeorm-database/src/Rdate/randomDate.ts
@@ -1,6 +1,5 @@
 import randomDate from '@js-random/date';
 
-var date = randomDate();
 var Rdate = randomDate({
     from: new Date(2021, 8, 8),
     to: new Date(2022, 8, 8) 
