621e27ab3b702bc31f51cff5ffaef8666ecf69fd
Iggna <iggnaa@gmail.com>
2021-08-06 22:30:44 -0300

    added schemas for validation

--- typeorm-database/src/Joi/validation_schema.js
+++ typeorm-database/src/Joi/validation_schema.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
-exports.authEmail = exports.authSchema = void 0;
+exports.authAddress = exports.authName = exports.authPage = exports.authEmail = exports.authSchema = void 0;
 var joi_1 = __importDefault(require("joi"));
 exports.authSchema = joi_1["default"].object({
     code: joi_1["default"].string().alphanum().equal(8)
@@ -11,3 +11,13 @@ exports.authSchema = joi_1["default"].object({
 exports.authEmail = joi_1["default"].object({
     customer_email: joi_1["default"].string().email().required()
 });
+exports.authPage = joi_1["default"].object({
+    page: joi_1["default"].number().required()
+});
+exports.authName = joi_1["default"].object({
+    name: joi_1["default"].string().required()
+});
+exports.authAddress = joi_1["default"].object({
+    address: joi_1["default"].string().required()
+});
+//preguntar si se pueden hacer menos validaciones
--- typeorm-database/src/Joi/validation_schema.ts
+++ typeorm-database/src/Joi/validation_schema.ts
@@ -8,3 +8,16 @@ export const authEmail = Joi.object({
     customer_email: Joi.string().email().required()
 })
 
+export const authPage = Joi.object({
+    page: Joi.number().required()
+})
+
+export const authName = Joi.object({
+    name: Joi.string().required()
+})
+
+export const authAddress = Joi.object({
+    address: Joi.string().required()
+})
+
+//preguntar si se pueden hacer menos validaciones
