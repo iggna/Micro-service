37c9ccbf965a1f128970c11d982b821b30abdc7f
Iggna <iggnaa@gmail.com>
2021-08-07 23:32:22 -0300

    started GET from stats

--- typeorm-database/src/controllers/stats.controller.ts
+++ typeorm-database/src/controllers/stats.controller.ts
@@ -1,1 +1,14 @@
-import { Store } from "../entity/store";
+import express, { Response, Request } from "express";
+import {authSchema, authEmail} from "../Joi/validation_schema";
+import Rdate from "../Rdate/randomDate";
+import { Coupon } from "../entity/coupon";
+import { FindConditions, getRepository, Equal } from "typeorm";
+
+export const getStats = async function (req: Request, res: Response) {
+    const repository = getRepository(Coupon);
+
+    // const existingCoupons = repository.findAndCount(id)
+    // const assignedCoupons = repository.findAndCount({where: {assignedAt !== undefined}})
+    // const NOTassignedCoupons = repository.findAndCount({where: {assignedAt === undefined}})
+    // const DAYassignedCoupons = repository.findAndCount({where: {assignedAt === undefined}})
+}
